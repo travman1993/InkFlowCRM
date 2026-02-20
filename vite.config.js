5749be60db3358d1b1fcf983c7c972b997e6de03
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-02-20 12:51:54 -0500

    Fix Vercel build: simplify vite config, add SPA rewrites

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [], // prevent React from being externalized
-    },
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true,
-    },
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react/jsx-runtime'],
-  },
 })
