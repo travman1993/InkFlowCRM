0666eacdd03b1aeb8f6ea2f530ab0cca70f843f8
Travis Rodriguez <travisgolembiewski@gmail.com>
2026-02-20 22:24:56 -0500

    p

---
+++ .vendor/react-jsx-runtime.js
@@ -0,0 +1,11 @@
+// <stdin>
+import _ext_react from "react";
+import _mod from "react/jsx-runtime";
+var { Fragment, jsx, jsxs } = _mod;
+var stdin_default = _mod;
+export {
+  Fragment,
+  stdin_default as default,
+  jsx,
+  jsxs
+};
